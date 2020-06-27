8cf2d4ea6c6c9fdd2fc7064a4db286fff324bc53
Md Rokonuzzaman Reza <44805545+EmonRezaBD@users.noreply.github.com>
2020-06-27 18:14:01 +0600

    Create 1326A codeforces solution.cpp

---
+++ 1326A codeforces solution.cpp
@@ -0,0 +1,62 @@
+#include<stdio.h>
+#include<iostream>
+#include<ctype.h>
+#include<cstring>
+#include<vector>
+#include<algorithm>
+#include<stdlib.h>
+#include<queue>
+#include<list>
+#include<set>
+#include<algorithm>
+#include<string>
+#include<stack>
+#include<cmath>
+#include<map>
+ 
+using namespace std;
+#define              ll              long long int
+#define              ff              first
+#define              ss              second
+#define              arsort(ar,n)    sort(ar,ar+n);
+#define              vsort(v)        sort(v.begin(),v.end())
+#define              vrev(v)         reverse(v.begin(),v.end())
+#define              arrev(ar,n)     reverse(ar,ar+n)
+#define              pb              push_back
+#define              popb            pop_back
+#define              fast ios_base::sync_with_stdio(0),cin.tie(0),cout.tie(0)
+#define              for0(i,n)       for(long long int i=0;i<n;i++)
+ 
+ 
+int main()
+{
+ 
+//	freopen("input.txt","r",stdin);
+	fast;
+	int t;
+	cin>>t;
+	//ll x,y;
+	ll n;
+	while (t--)
+	{
+		/* code */
+		cin>>n;
+		if(n==1)
+		   cout<<-1<<endl;
+		else
+		{
+			cout<<5<<"";
+			n--;
+			for (int i = 0; i < n; i++)
+			{
+				/* code */
+				cout<<7; //prime num should be generated.
+			}
+			cout<<endl;
+			
+		}
+		
+	}
+	
+	return 0;
+}
